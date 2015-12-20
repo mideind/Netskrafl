29c99388b0a27bf7979b940e4c4e7ccd3b9f981c
Steinar Sturlaugsson <steinar.sturlaugsson@gmail.com>
2015-12-20 01:41:13 +0100

    Script for creating flask secret

---
+++ generate-secret.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import os
+import sys
+
+file_path="resources/secret_key.bin"
+
+def generate():
+    print("Generating session token, using os.urandom")
+    bytes=os.urandom(64)
+    with open(file_path, "wb") as f:
+        f.write(bytes)
+
+def read():
+    print("Contents of {}".format(file_path))
+    with open(file_path, "rb") as f:
+        bytes = f.read()
+        print(bytes.encode('base64'))
+
+if len(sys.argv) > 1 and sys.argv[1] == "read":
+    read()
+else:
+    generate()
+
