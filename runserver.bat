b8ab425bf846bf76e049b6370cdd282a59d2ac57
Vilhjalmur Thorsteinsson <vt@extrada.com>
2019-10-31 22:48:12 +0000

    New version of jquery.ui.touch-punch.js; batch files and .gitignore updated

--- runserver.bat
+++ runserver.bat
@@ -1,3 +1,6 @@
 REM Start the Google App Engine Development Server for Python
 set GOOGLE_APPLICATION_CREDENTIALS=resources\netskrafl-0dd9fbdf9ab3.json
-python dev_appserver.py --port=8080 --admin_port=8000 --host=0.0.0.0 --enable_host_checking=False app.yaml skraflstats.yaml
+set APPSERVER="\program files (x86)\google\google_appengine\dev_appserver.py"
+set PYTHONEXE=\python27\python
+set PYTHONIOENCODING=utf-8
+%PYTHONEXE% %APPSERVER% --port=8080 --admin_port=8000 --host=0.0.0.0 --enable_host_checking=False app.yaml skraflstats.yaml
