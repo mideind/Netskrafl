a1731276679f0722dab9847ca9202ea8986f1f6a
Vilhjalmur Thorsteinsson <vt@extrada.com>
2020-01-08 14:56:05 +0000

    Copied runserver.bat from singlepage branch

--- runserver.bat
+++ runserver.bat
@@ -2,7 +2,6 @@ REM Start the Google App Engine Development Server for Python
 set GOOGLE_APPLICATION_CREDENTIALS=resources\netskrafl-0dd9fbdf9ab3.json
 set PYTHON=C:\python27\python
 set CLOUD_SDK=%LOCALAPPDATA%\Google\Cloud SDK\google-cloud-sdk
-set PYTHONPATH=%CLOUD_SDK%\platform\google_appengine
+set PYTHONPATH=%CLOUD_SDK%\platform\google_appengine;%cd%\lib
 set DEV_APPSERVER=%CLOUD_SDK%\bin\dev_appserver.py
-set PYTHONIOENCODING=utf-8
-%PYTHON% "%DEV_APPSERVER%" --port=8080 --admin_port=8000 --host=0.0.0.0 --enable_host_checking=False app.yaml skraflstats.yaml
+%PYTHON% "%DEV_APPSERVER%" --port=8080 --admin_port=8000 --host=0.0.0.0 --enable_console --enable_host_checking=False app.yaml skraflstats.yaml
