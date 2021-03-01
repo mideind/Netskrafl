9d4d0f8820d4edbe8bd09dd14e9237694bfdf383
Tomasz Bartyzel <tbartyzel@MacBook-Pro-Tomasz-2.local>
2021-03-01 10:01:51 +0100

    config app for explo

--- runserver.sh
+++ runserver.sh
@@ -1,1 +1,12 @@
-dev_appserver.py --skip_sdk_update_check=yes --port=8080 --admin_port=8000 --host=0.0.0.0 app.yaml skraflstats.yaml
+#!/bin/bash
+
+export GOOGLE_APPLICATION_CREDENTIALS=./resources/Explo\ Development-414318fa79b8.json
+export PROJECT_ID=explo-dev
+export CLIENT_ID=970204261331-758cjav6i4lbiq1nemm6j8215omefqg3.apps.googleusercontent.com
+export FIREBASE_API_KEY=AIzaSyCsNVCzDnAXo_cbViXl7fa5BYr_Wz6lFEc
+export FIREBASE_SENDER_ID=970204261331
+export FIREBASE_APP_ID=1:970204261331:web:fce1615824c2e382ec9d26
+export SERVER_SOFTWARE=Development
+export OAUTHLIB_INSECURE_TRANSPORT=1
+
+python3 ./netskrafl.py
