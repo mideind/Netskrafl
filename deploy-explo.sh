7261287f97bb523509a59d961130c171e3af54fd
Vilhjalmur Thorsteinsson <vt@extrada.com>
2021-07-12 16:53:51 +0000

    New Explo deployment script; updated app-explo.yaml

---
+++ deploy-explo.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo -e "Deploy an update to Explo Development App Server\n"
+
+export GOOGLE_APPLICATION_CREDENTIALS=./resources/Explo\ Development-414318fa79b8.json
+export PROJECT_ID=explo-dev
+
+cmd=$1
+version=$2
+
+function stats
+{
+    echo "Skraflstats deployment starting"
+    echo "*** Currently disabled ***"
+    echo "Skraflstats deployment completed"
+}
+
+function indexes
+{
+    echo "Index update starting"
+    echo "*** Currently disabled ***"
+    echo "Index update completed"
+}
+
+function cron
+{
+    echo "Cron update starting"
+    echo "*** Currently disabled ***"
+    echo "Cron update completed"
+}
+
+function default # $1=version
+{
+    if [[ $1 == "" ]]; then
+        echo "Version is missing; enter deploy d[efault] <version>"
+        exit 1
+    fi
+    echo "Default module deployment starting, version '$1'"
+    grunt make
+    gcloud beta app deploy --version=$1 --no-promote --project=explo-dev app-explo.yaml
+    echo "Default module deployment completed"
+}
+
+case $cmd in
+    skraflstats|stats|s)
+        stats
+        ;;
+    indexes|ix|i)
+        indexes
+        ;;
+    cron|c)
+        cron
+        ;;
+    default|d)
+        default "$version"
+        ;;
+    *)
+        echo "Please enter $0 (stats|indexes|cron|default <version>)"
+        exit 1
+        ;;
+esac
