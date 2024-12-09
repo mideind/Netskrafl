4bcdf3b97db0a0e60692bfb532f9d28fb544d543
Vilhjalmur Thorsteinsson <vt@extrada.com>
2024-12-09 11:05:02 +0000

    Updated CORS origin list to include port 3001 for development

--- src/main.py
+++ src/main.py
@@ -135,8 +135,10 @@ if running_local:
         supports_credentials=True,
         origins=[
             "http://127.0.0.1:3000",
+            "http://127.0.0.1:3001",
             "http://127.0.0.1:6006",
             "http://localhost:3000",
+            "http://localhost:3001",
             "http://localhost:6006",
         ],
     )
